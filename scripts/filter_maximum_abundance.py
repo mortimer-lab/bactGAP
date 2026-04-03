986a9091ecb8480c4c9084db9fbd00671a1b4ee0
Selina <ss61143@uga.edu>
2026-04-03 14:28:59 -0400

    script max abundance edits

--- scripts/filter_maximum_abundance.py
+++ scripts/filter_maximum_abundance.py
@@ -7,19 +7,25 @@ def get_args():
     parser = argparse.ArgumentParser(description='Use filtered TSV files in pandas script')
     parser.add_argument("input", help="Insert TSV file")
     parser.add_argument("output", help="Output TSV file")
+    parser.add_argument("--genus", required=True)
+    parser.add_argument("--species", required=True)
     return parser.parse_args()
+
+# Check if the species is desired species
+def filter_species(df, genus_name, species_name):
+    x = pd.Series(True, index=df.index)
+    x &= df['Contig_name'].str.contains(f"{genus_name} {species_name}", case=False, na=False)
+    return df[x]
 
 def main():
     args = get_args()
+    df = filter_species(df, args.genus, args.species)
 
 # Read CSV file
     df = pd.read_csv(args.input, sep='\t')
 
 # Filter the DataFrame
     filtered_df = df[df['Taxonomic_abundance'] > 96.0]
-
-# Check if the species is desired species
-    
 
 # Save to TSV file
     filtered_df.to_csv(args.output, sep='\t', index=False)
